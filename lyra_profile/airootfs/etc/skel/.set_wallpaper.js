ffc4f154d8a00a75bd9dc2f7ac082ac1ead41253
Rodrigo Brito <rodrigo@w3ti.com.br>
2026-06-10 08:55:23 -0300

    chore: Limpando o diretorio

--- lyra_profile/airootfs/etc/skel/.set_wallpaper.js
+++
@@ -1,8 +0,0 @@
-var allDesktops = desktops();
-print (allDesktops);
-for (var i=0;i<allDesktops.length;i++) {
-    d = allDesktops[i];
-    d.wallpaperPlugin = "org.kde.image";
-    d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");
-    d.writeConfig("Image", "file:///usr/share/wallpapers/lyra/lyra-cosmos.svg");
-}
